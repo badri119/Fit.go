b9344c0c10aa68ce78b39522e1484bf062937602
badri119 <badrinathv98@gmail.com>
2022-10-10 20:37:15 -0700

    initial stage

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+
+
+function HomeScreen({navigation}) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Fit.go</Text>
+      <Text>Start Swiping to find your workout partner!</Text>
       <StatusBar style="auto" />
+      <Button title="Login" onPress={() => navigation.navigate('Login')}/>
+      <Button title="Sign Up" onPress={() => navigation.navigate('Sign Up')}/>
     </View>
+    
+  );
+}
+
+function SignUpScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Sign Up!</Text>
+      <Button title="Go Back" onPress={() => navigation.navigate('Home')} />
+    </View>
+  );
+}
+
+function LoginScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Sign in!</Text>
+      <Button title="Go Back" onPress={() => navigation.navigate('Home')} />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Sign Up" component={SignUpScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -16,5 +56,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white',
   },
 });
+
+export default App;
+
+
